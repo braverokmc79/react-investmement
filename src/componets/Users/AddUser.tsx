91554656f5f6a43d865ad06b0c7302e1bc784f38
braverokmc79 <braverokmc79@gmail.com>
2024-06-07 18:51:38 +0900

    687.재사용 가능한 '카드' 컴포넌트 추가하기

--- src/componets/Users/AddUser.tsx
+++ src/componets/Users/AddUser.tsx
@@ -1,20 +1,29 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser: React.FC = () => {
-  const adduserHandler = (event:React.FormEvent) => {
+  const adduserHandler = (event: React.FormEvent) => {
     event.preventDefault();
   };
 
-
   return (
-    <form onSubmit={adduserHandler}>
-      <label htmlFor="username">유저명</label>
-      <input id="username" type="text" />
+    <Card className={classes.input}>
+      <form onSubmit={adduserHandler}>
+        <label htmlFor="username" className="text-label">유저명</label>
+        <input id="username" type="text" />
 
-      <label htmlFor="age">나에</label>
-      <input id="age" type="numbre" />
-      <button type="submit" >유저추가</button>
-    </form>
+        <br/>
+        <label htmlFor="age" className="text-label">나이</label>
+        <input id="age" type="numbre" />
+
+        <div className="text-center">
+          <button type="submit" className="button">
+            유저추가
+          </button>
+        </div>
+      </form>
+    </Card>
   );
 };
 
